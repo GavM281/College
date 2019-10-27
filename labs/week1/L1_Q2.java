103929dc1328c22f20062577f181616ec7d8b120
Gavin M <gavin1mulhern@gmail.com>
2019-10-27 17:40:55 +0000

    Added Lab1 questions.

---
+++ labs/week1/L1_Q2.java
@@ -0,0 +1,7 @@
+public class Printing3 {
+    public static void main(String args[]) {
+        System.out.println("3 + 4"); //3+4
+        System.out.println(3 + 4); // 7
+        System.out.println(3 + "4"); // 34
+    }
+}
---
+++ labs/week1/L1_Q4.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String args[]) {
+        int num1 = 10;
+        int num2 = 5;
+        int result1 = 0;
+        int result2 = 0;
+        double result3 = 0.0;
+        result1 = num1 + num2; // 10 + 5
+        result2 = num1 * num2; // 10 * 5
+        result3 = num1 / num2; // 10 / 2
+        System.out.println("Addition: " + result1); //Addition: 15
+        System.out.println("Multiplication: " + result2); //Multiplication: 50
+        System.out.println("Division: " + result3); //Division: 2.0 
+    }
+}
