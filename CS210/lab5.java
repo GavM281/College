686090057255a6c10008175753f8372079ce7a7d
Gavin M <gavin1mulhern@gmail.com>
2021-08-27 22:59:54 +0100

    Added improved code
    Original code used for lab had a lot of repetition when checking whether to swap words of the same length. Added code that reduces the repetition.

--- CS210/lab5.java
+++ CS210/lab5.java
@@ -65,3 +65,56 @@ public class lab5 {
         System.out.println(array[select]); //print word in selected position     
     }
 }
+
+
+
+
+// New version that reduces the repetition of code used for lab
+
+// import java.util.*;
+// public class lab5 {
+//     public static void main(String args[]) {
+//         Scanner scan = new Scanner(System.in);
+//         int len = scan.nextInt();
+//         len++;
+//         int select = scan.nextInt();
+
+//         //create array of len length
+//         String array[] = new String[len];
+        
+//         //for loop fill in array with inputted words
+//         for(int i = 0; i<len; i++){
+//             array[i] = scan.nextLine();
+//         }
+        
+//         //sort using bubble sort
+//         int outer, inner;
+//         for(outer=len-1; outer > 0; outer--){
+//             for(inner=0;inner < outer; inner++){
+//                 if(array[inner].length() > array[inner+1].length()){ //if inner is longer than inner+1
+//                     String temp = array[inner];
+//                     array[inner] = array[inner+1];
+//                     array[inner+1] = temp;
+                    
+//                 }else if(array[inner].length() == array[inner+1].length()){ //if lengths are the same
+//                     array = check(array[inner].length(),inner, array);
+//                 }
+//             }
+//         }
+//         System.out.println(array[select]); //print word in selected position     
+//     }
+    
+//     public static String[] check(int len,int inner, String arr[]){
+//         for(int i=0; i<len-1;i++){ // Go through word length
+//             char in = arr[inner].charAt(i); //get letter i for word at index inner
+//             char inPlus = arr[inner+1].charAt(i); //get letter i for word at index inner+1
+            
+//             if(inPlus>in){ // If letter at inner+1 is bigger swap the words
+//                 String temp = arr[inner];
+//                 arr[inner] = arr[inner+1];
+//                 arr[inner+1] = temp;
+//             }   
+//         }
+//         return arr; // Return array
+//     }
+// }
